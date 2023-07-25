e64771297844bc323dc57503e0ac797d69f243b2
emeibech <98073805+emeibech@users.noreply.github.com>
2023-07-25 16:24:44 +0800

    Add sun icon to header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,12 +1,19 @@
 import { cn } from "@/lib/utils";
 import SiteTitle from "./SiteTitle";
-import Navigation from "../Navigation/Navigation";
+import NavigationSheet from "../Navigation/NavigationSheet";
+import { SunIcon } from "@/components/custom/Icons";
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
   return (
-    <header className={cn("flex items-center gap-2 lg:hidden")}>
-      <Navigation side="left" />
-      <SiteTitle />
+    <header className={cn("flex justify-between lg:justify-end")}>
+      <section className="lg:hidden flex items-center gap-2 min-h-[40px]">
+        <NavigationSheet side="left" />
+        <SiteTitle />
+      </section>
+      <Button variant="ghost" type="button" className="px-2">
+        <SunIcon height="24px" />
+      </Button>
     </header>
   );
 }
