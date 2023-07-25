437175f4c5c6739b82c2329173a0468d6080a9a3
emeibech <98073805+emeibech@users.noreply.github.com>
2023-07-25 16:25:30 +0800

    Restructure elements and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import "./App.css";
 import Header from "./layout/Header/Header";
+import Nav from "./layout/Navigation/Nav";
+import SiteTitle from "./layout/Header/SiteTitle";
+import { Separator } from "@/components/ui/separator";
 
 function App() {
   return (
     <>
-      <Header />
+      <section className="lg:grid lg:grid-cols-[240px_1fr]">
+        <section className="lg:flex justify-between hidden">
+          <div className="flex-col flex-1">
+            <SiteTitle />
+            <Nav />
+          </div>
+          <Separator orientation="vertical" />
+        </section>
+        <section className="flex flex-col">
+          <Header />
+          <Separator className="my-2" />
+        </section>
+      </section>
     </>
   );
 }
