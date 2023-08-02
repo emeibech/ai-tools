02d09bd2ddf0b1ec845041113a0d86a9d69c5917
emeibech <98073805+emeibech@users.noreply.github.com>
2023-08-02 14:25:14 +0800

    Simplify code for rendering list for nav

--- src/layout/navigation/Nav.tsx
+++ src/layout/navigation/Nav.tsx
@@ -1,27 +1,27 @@
 import { cn } from "@/lib/utils";
 
 export default function Nav() {
+  const list = [
+    "Code Analyzer",
+    "Coding Assistant",
+    "Eli5",
+    "Lore Generator",
+    "Tone Changer",
+    "General Assistant",
+  ];
+  const listItems = list.map((item) => (
+    <li
+      key={item}
+      className={cn("px-4 py-2 rounded text-sm", "hover:bg-secondary")}
+    >
+      {item}
+    </li>
+  ));
+
   return (
     <nav className={cn("min-h-screen mt-12", "lg:px-6 lg:mt-40")}>
       <ul className={cn("flex flex-col gap-1 cursor-pointer", "lg:gap-2")}>
-        <li className={cn("px-4 py-2 rounded text-sm", "hover:bg-secondary")}>
-          Code Analyzer
-        </li>
-        <li className={cn("px-4 py-2 rounded text-sm", "hover:bg-secondary")}>
-          Coding Assistant
-        </li>
-        <li className={cn("px-4 py-2 rounded text-sm", "hover:bg-secondary")}>
-          ELI5
-        </li>
-        <li className={cn("px-4 py-2 rounded text-sm", "hover:bg-secondary")}>
-          Lore Generator
-        </li>
-        <li className={cn("px-4 py-2 rounded text-sm", "hover:bg-secondary")}>
-          Tone Changer
-        </li>
-        <li className={cn("px-4 py-2 rounded text-sm", "hover:bg-secondary")}>
-          General Assistant
-        </li>
+        {listItems}
       </ul>
     </nav>
   );
