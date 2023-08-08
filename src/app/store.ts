a3777435af1d821dea9502eaf4372ccec73a01f0
emeibech <98073805+emeibech@users.noreply.github.com>
2023-08-09 01:35:56 +0800

    Set up store

---
+++ src/app/store.ts
@@ -0,0 +1,17 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import darkmodeReducer from "../features/darkmode/darkmodeSlice";
+
+export const store = configureStore({
+  reducer: {
+    darkmodeToggler: darkmodeReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
