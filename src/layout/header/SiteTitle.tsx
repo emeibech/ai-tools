f35bc0bde9e541fc5bd7d031e3d2aff6f8035a7f
emeibech <98073805+emeibech@users.noreply.github.com>
2023-08-15 06:44:30 +0800

    Wrap the entire component in Link

--- src/layout/header/SiteTitle.tsx
+++ src/layout/header/SiteTitle.tsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 export default function SiteTitle() {
   return (
-    <h1
-      data-name="site-name"
-      className={cn(
-        "text-xl text-left cursor-pointer font-bold rounded-md px-2",
-        "lg:text-2xl lg:px-6 lg:py-1",
-      )}
-    >
-      <Link to={"/"}>
+    <Link to={"/"}>
+      <h1
+        data-name="site-name"
+        className={cn(
+          "text-xl text-left cursor-pointer font-bold rounded-md px-2",
+          "lg:text-2xl lg:px-6 lg:py-1",
+        )}
+      >
         emeibech<span className={cn("ml-1")}>ai</span>
-      </Link>
-    </h1>
+      </h1>
+    </Link>
   );
 }
