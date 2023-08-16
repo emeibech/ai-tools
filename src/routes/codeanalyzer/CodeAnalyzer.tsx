57204f1649e2c282820936cc337e1e467252e114
emeibech <98073805+emeibech@users.noreply.github.com>
2023-08-17 04:43:36 +0800

    Make the textarea functional

--- src/routes/codeanalyzer/CodeAnalyzer.tsx
+++ src/routes/codeanalyzer/CodeAnalyzer.tsx
@@ -19,7 +19,7 @@ export default function CodeAnalyzer() {
         Code Analyzer
       </h2>
 
-      <p className="max-w-[640px]">
+      <p>
         Code Analyzer helps you understand complex snippets of code or
         not-so-complex-but-hard-to-read ones written by noobs like me. Input the
         code in the box, submit, and you will get a response giving you a brief
--- src/routes/codeanalyzer/CodeAnalyzerForm.tsx
+++ src/routes/codeanalyzer/CodeAnalyzerForm.tsx
@@ -1,29 +1,45 @@
+import { useRef, useState } from "react";
 import { Textarea } from "@/common/components/ui/textarea";
 import { Button } from "@/common/components/ui/button";
+import { SendIcon } from "@/common/components/ui/Icons";
 import { cn } from "@/common/lib/utils";
 
 export default function CodeAnalyzerForm() {
-  function onSubmit(values: string) {
-    console.log(values);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const codeRef = useRef<HTMLTextAreaElement>(null);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    console.log(codeRef.current?.value);
+    codeRef.current!.value = "";
+    setIsButtonDisabled(true);
+  }
+
+  function handleInputChange() {
+    const inputLength = codeRef.current?.value.length ?? 0;
+    setIsButtonDisabled(inputLength <= 4);
   }
 
   return (
-    <form onSubmit={() => onSubmit} className="grid gap-2 relative">
+    <form onSubmit={handleSubmit} className="grid gap-2 relative">
       <Textarea
+        onChange={handleInputChange}
+        ref={codeRef}
         placeholder="Put your code here"
         rows={12}
         cols={100}
-        wrap="hard"
         className="resize-none"
       />
       <Button
-        type="submit"
+        disabled={isButtonDisabled}
+        variant={"ghost"}
         className={cn(
-          "justify-self-end max-w-max",
-          "absolute bottom-2 right-2",
+          "justify-self-end max-w-max px-2.5 rounded-full",
+          "absolute bottom-3 right-3",
+          "transition-color transition-opacity duration-300",
         )}
       >
-        Submit
+        <SendIcon height="20px" />
       </Button>
     </form>
   );
