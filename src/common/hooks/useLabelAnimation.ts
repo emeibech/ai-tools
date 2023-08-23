22f316661f9674cfe0b94d402c8e9e89d2cfff1c
emeibech <98073805+emeibech@users.noreply.github.com>
2023-08-24 05:30:03 +0800

    Refactor hook to handle more logic for animation

--- src/common/hooks/useLabelAnimation.ts
+++ src/common/hooks/useLabelAnimation.ts
@@ -1,50 +1,78 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-type FieldState = "blurred" | "focused" | "dirty";
+export type FieldState =
+  | "default"
+  | "focusedValid"
+  | "focusedInvalid"
+  | "blurredValid"
+  | "blurredInvalid";
+
+interface useLabelAnimationProps {
+  isDirty: boolean;
+  isInvalid: boolean;
+}
 
 /**
  * Abstracts the animation of a label. This hook makes it possible to pick and choose
  * which label you want to animate. The animation includes going up, down, or remain
- * up depending on these input events: onFocus, onBlur, onChange.
- * @param {boolean} isDirty - The property from getFieldState method
+ * up depending on these input events.
  */
-export default function useLabelAnimation(isDirty: boolean) {
-  const [fieldState, setFieldState] = useState<FieldState>("blurred");
+export default function useLabelAnimation({
+  isDirty,
+  isInvalid,
+}: useLabelAnimationProps) {
+  const [fieldState, setFieldState] = useState<FieldState>("default");
 
-  function floatUp() {
-    if (!isDirty) setFieldState("focused");
-  }
+  const onFocus = () => {
+    setFieldState(isInvalid ? "focusedInvalid" : "focusedValid");
+  };
 
-  function floatDown() {
-    if (!isDirty) setFieldState("blurred");
-  }
-
-  function remainUp() {
-    setFieldState("dirty");
-  }
-
-  function resetState() {
-    setFieldState("blurred");
-  }
-
-  function getFieldState(): FieldState {
-    return fieldState;
-  }
-
-  function getStyle(): string {
-    if (fieldState === "blurred") {
-      return "translate-y-5 -translate-x-[3px] text-muted-foreground";
+  const onBlur = () => {
+    if (isInvalid) {
+      setFieldState("blurredInvalid");
+    } else if (isDirty) {
+      setFieldState("blurredValid");
+    } else {
+      setFieldState("default");
     }
+  };
 
-    return "translate-y-0 -translate-x-0";
-  }
+  useEffect(() => {
+    if (isDirty) {
+      setFieldState(isInvalid ? "focusedInvalid" : "focusedValid");
+    } else if (isInvalid) {
+      setFieldState("blurredInvalid");
+    }
+  }, [isDirty, isInvalid]);
+
+  const resetState = () => {
+    setFieldState("default");
+  };
+
+  const getFieldState = (): FieldState => {
+    return fieldState;
+  };
+
+  const getAnimationStyles = (): string => {
+    switch (fieldState) {
+      case "focusedInvalid":
+        return "translate-y-0 -translate-x-1 scale-90 text-destructive";
+      case "focusedValid":
+        return "translate-y-0 -translate-x-1 scale-90 text-foreground transition-transform";
+      case "blurredInvalid":
+        return "translate-y-0 -translate-x-1 scale-90 text-destructive transition-transform";
+      case "blurredValid":
+        return "translate-y-0 -translate-x-1 scale-90 text-muted-foreground transition-transform";
+      default:
+        return "translate-y-5 text-muted-foreground transition-transform";
+    }
+  };
 
   return {
-    floatUp,
-    floatDown,
-    remainUp,
+    onFocus,
+    onBlur,
     resetState,
     getFieldState,
-    getStyle,
+    getAnimationStyles,
   };
 }
