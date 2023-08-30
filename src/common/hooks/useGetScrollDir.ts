3ec7766e0634f5f911431968e60137cc01920b67
emeibech <98073805+emeibech@users.noreply.github.com>
2023-08-31 07:29:18 +0800

    Create hook to get scroll direction

---
+++ src/common/hooks/useGetScrollDir.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function useGetScrollDir() {
+  const [scrollDirection, setScrollDirection] = useState("");
+
+  useEffect(() => {
+    let prevScrollPos = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      if (prevScrollPos > currentScrollPos) {
+        setScrollDirection("up");
+      } else {
+        setScrollDirection("down");
+      }
+      prevScrollPos = currentScrollPos;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return scrollDirection;
+}
