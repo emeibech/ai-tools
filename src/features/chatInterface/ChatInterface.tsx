b73524e771b6526b490f8f606369220ed910dae5
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-06 22:27:45 +0800

    Integrate with useScrollToNewMessage
    -Add ref callback to ChatMessage
    -Also accepts children to be displayed as initial message and is not
      included in the messages state

--- src/features/chatInterface/ChatInterface.tsx
+++ src/features/chatInterface/ChatInterface.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { cn, generateKeys } from "@/common/lib/utils";
 import useGetScrollDir from "@/common/hooks/useGetScrollDir";
 import ChatMessage from "./ChatMessage";
 import ChatInterfaceForm from "./ChatInterfaceForm";
+import useScrollToNewMessage from "@/common/hooks/useScrollToNewMessage";
 
 interface ChatInterfaceProps {
   name: string;
-  initialMessage: string;
+  children: ReactNode;
 }
 
 export interface Messages {
@@ -15,19 +16,30 @@ export interface Messages {
   content: string;
 }
 
-export default function ChatInterface({ name }: ChatInterfaceProps) {
+export default function ChatInterface({ name, children }: ChatInterfaceProps) {
   const scrollDirection = useGetScrollDir();
   const [messages, setMessages] = useState<Messages[]>([]);
   const messagesKeys = generateKeys(messages);
+  const getMap = useScrollToNewMessage(messages);
 
-  const listMessages = messages.map((message, index) => (
-    <ChatMessage key={messagesKeys[index]} data-id={messagesKeys[index]}>
-      ID: {message.id}
-      <br />
-      <br />
-      Message: {message.content}
-    </ChatMessage>
-  ));
+  const listMessages: JSX.Element[] = messages.map((message, index) => {
+    return (
+      <ChatMessage
+        key={messagesKeys[index]}
+        data-id={messagesKeys[index]}
+        ref={(node: HTMLElement | null) => {
+          const map = getMap();
+          if (node) {
+            map.set(message.id, node);
+          } else {
+            map.delete(message.id);
+          }
+        }}
+      >
+        {message.content}
+      </ChatMessage>
+    );
+  });
 
   return (
     <section className="relative max-w-[1024px]">
@@ -44,50 +56,7 @@ export default function ChatInterface({ name }: ChatInterfaceProps) {
       </div>
 
       <section className={cn("min-h-[85vh] max-w-[768px] mx-auto")}>
-        <ChatMessage>
-          The Snake and the Farmer: This story often attributed to Lincoln
-          involves a farmer who found a frozen snake and brought it home. After
-          reviving the snake by the fire, it bit the farmer's wife. When the
-          farmer asked why, the snake replied, "You knew I was a snake when you
-          picked me up." Lincoln used this story to illustrate the dangers of
-          trusting those who have shown themselves to be untrustworthy.
-        </ChatMessage>
-        <ChatMessage>
-          The Judge's Ruling: Lincoln would tell a story about a judge who made
-          a confusing and contradictory ruling in a case. When asked about it,
-          the judge replied, "I have a right to change my mind; I'm not a bound
-          man." Lincoln used this story to highlight the importance of
-          consistency and fairness in judgment.
-        </ChatMessage>
-        <ChatMessage>
-          The Dog's Tail: Lincoln would tell a tale about a man who had his
-          dog's tail cut off bit by bit each day, thinking it wouldn't hurt the
-          dog as much as cutting it all at once. Lincoln used this story to
-          discuss the issue of slavery, comparing it to the gradual abolition of
-          the institution.
-        </ChatMessage>
-        <ChatMessage>
-          The Two-Faced Calf: Lincoln often told a humorous story about a farmer
-          who had a two-faced calf—one head at each end. When visitors asked how
-          it ate, the farmer replied, "I don't rightly know, but it gets there."
-          Lincoln used this story to humorously illustrate the idea that the
-          country would get through its challenges, even if things seemed odd or
-          difficult.
-        </ChatMessage>
-        <ChatMessage>
-          The Misplaced Knife: Lincoln would recount a story about a man who
-          claimed his knife had the same blade for 40 years, having only
-          replaced the handle 10 times and the blade 5 times. He used this story
-          to talk about change and how sometimes what's essential remains even
-          if the outward appearance changes.
-        </ChatMessage>
-        <ChatMessage>
-          The Borrowed Ax: Lincoln would share a story about a man who borrowed
-          his neighbor's ax and ended up breaking it. Instead of denying
-          responsibility, he admitted to it and offered to either fix or replace
-          the ax. This story exemplified Lincoln's belief in honesty and taking
-          responsibility for one's actions.
-        </ChatMessage>
+        {children}
         {listMessages}
       </section>
 
