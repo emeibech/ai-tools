05df9921ea336d43df61c4637850dd7125cfb9db
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-06 22:16:50 +0800

    Refactor to accept ref

--- src/features/chatInterface/ChatMessage.tsx
+++ src/features/chatInterface/ChatMessage.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/common/components/ui/button";
 import { cn } from "@/common/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { MinusCircle } from "lucide-react";
-import { ReactNode } from "react";
+import { ReactNode, forwardRef } from "react";
 import chatBot from "./imgs/chatbot.png";
 import { Separator } from "@/common/components/ui/separator";
 
@@ -10,50 +10,55 @@ interface ChatMessageProps {
   children: ReactNode;
 }
 
-export default function ChatMessage({ children }: ChatMessageProps) {
-  return (
-    <>
-      <article
-        className={cn(
-          " min-h-[120px] min-w-full py-6",
-          "grid grid-cols-[32px_1fr]",
-          "min-[375px]:grid-cols-[40px_1fr_16px]",
-        )}
-      >
-        <Avatar className=" justify-self-center py-4">
-          <AvatarImage src={chatBot} className="bg-accent rounded-full" />
-          <AvatarFallback>AI</AvatarFallback>
-        </Avatar>
-
-        <p
+const ChatMessage = forwardRef<HTMLElement, ChatMessageProps>(
+  ({ children }, ref) => {
+    return (
+      <>
+        <article
+          ref={ref}
           className={cn(
-            " px-2 py-4 text-sm overflow-auto",
-            "min-[375px]:text-base sm:px-6",
+            " min-h-[120px] min-w-full py-6",
+            "grid grid-cols-[32px_1fr]",
+            "min-[375px]:grid-cols-[40px_1fr_16px]",
           )}
         >
-          {children}
-        </p>
+          <Avatar className=" justify-self-center py-4">
+            <AvatarImage src={chatBot} className="bg-accent rounded-full" />
+            <AvatarFallback>AI</AvatarFallback>
+          </Avatar>
 
-        <div
-          className={cn(
-            " col-span-2 row-span-1 justify-self-end self-center max-h-[1rem]",
-            "min-[375px]:col-span-1 min-[375px]:self-start",
-          )}
-        >
-          <Button
-            variant={"custom"}
-            size={"custom"}
+          <p
             className={cn(
-              "text-muted-foreground opacity-75",
-              "hover:text-destructive active:text-destructive",
+              " px-2 py-4 text-sm overflow-auto",
+              "min-[375px]:text-base sm:px-6",
             )}
           >
-            <MinusCircle width="16px" height="16px" />
-          </Button>
-        </div>
-      </article>
+            {children}
+          </p>
 
-      <Separator />
-    </>
-  );
-}
+          <div
+            className={cn(
+              " col-span-2 row-span-1 justify-self-end self-center max-h-[1rem]",
+              "min-[375px]:col-span-1 min-[375px]:self-start",
+            )}
+          >
+            <Button
+              variant={"custom"}
+              size={"custom"}
+              className={cn(
+                "text-muted-foreground opacity-75",
+                "hover:text-destructive active:text-destructive",
+              )}
+            >
+              <MinusCircle width="16px" height="16px" />
+            </Button>
+          </div>
+        </article>
+
+        <Separator />
+      </>
+    );
+  },
+);
+
+export default ChatMessage;
