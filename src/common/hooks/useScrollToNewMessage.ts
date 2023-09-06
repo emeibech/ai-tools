f9214244d1dc8fd92bbe27567986ca307dc3c675
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-06 22:12:06 +0800

    Create hook that scrolls to newest message

---
+++ src/common/hooks/useScrollToNewMessage.ts
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+import { Messages } from "@/features/chatInterface/ChatInterface";
+
+export default function useScrollToNewMessage(messages: Messages[]) {
+  const messagesRef = useRef<Map<string, HTMLElement>>(new Map());
+
+  function getMap() {
+    if (!messagesRef.current) {
+      messagesRef.current = new Map();
+    }
+    return messagesRef.current;
+  }
+
+  useEffect(() => {
+    function scrollToId(itemId: string) {
+      const map = getMap();
+      const node = map.get(itemId);
+      node?.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (messages) scrollToId(messages[messages.length - 1]?.id);
+  }, [messages]);
+
+  return getMap;
+}
