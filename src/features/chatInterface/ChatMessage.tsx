55e48e9d8e063b5cbdcb50f5f2aa520eead7989f
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-08 15:29:06 +0800

    Add logic for rendering code blocks
    -formatMessage extracts code blocks enclosed in ``` delimiter
    -it returns sorted jsx elements. Code blocks are contained in <Code>
      while non-code messages are contained in <p>

--- src/features/chatInterface/ChatMessage.tsx
+++ src/features/chatInterface/ChatMessage.tsx
@@ -1,13 +1,29 @@
-import { Button } from "@/common/components/ui/button";
-import { cn } from "@/common/lib/utils";
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { MinusCircle } from "lucide-react";
-import { ReactNode, forwardRef } from "react";
-import chatBot from "./imgs/chatbot.png";
-import { Separator } from "@/common/components/ui/separator";
+import { Button } from '@/common/components/ui/button';
+import { cn } from '@/common/lib/utils';
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
+import { MinusCircle } from 'lucide-react';
+import { ReactNode, forwardRef } from 'react';
+import chatBot from './imgs/chatbot.png';
+import { Separator } from '@/common/components/ui/separator';
+import Code from './Code';
 
 interface ChatMessageProps {
   children: ReactNode;
+}
+
+function formatMessage(message: ReactNode): JSX.Element {
+  const messageString = `${message}`;
+  const codeBlocks = messageString.split('```');
+
+  const formattedMessage = codeBlocks.map((block, index) => {
+    if (index % 2 === 0) {
+      return <p key={`text-${index}`}>{block}</p>;
+    } else {
+      return <Code key={`code-${index}`} code={block} />;
+    }
+  });
+
+  return <>{formattedMessage}</>;
 }
 
 const ChatMessage = forwardRef<HTMLElement, ChatMessageProps>(
@@ -17,9 +33,9 @@ const ChatMessage = forwardRef<HTMLElement, ChatMessageProps>(
         <article
           ref={ref}
           className={cn(
-            " min-h-[120px] min-w-full py-6",
-            "grid grid-cols-[32px_1fr]",
-            "min-[375px]:grid-cols-[40px_1fr_16px]",
+            'min-h-[120px] min-w-full py-6',
+            'grid grid-cols-[32px_1fr]',
+            'min-[375px]:grid-cols-[40px_1fr_16px]'
           )}
         >
           <Avatar className=" justify-self-center py-4">
@@ -27,27 +43,27 @@ const ChatMessage = forwardRef<HTMLElement, ChatMessageProps>(
             <AvatarFallback>AI</AvatarFallback>
           </Avatar>
 
-          <p
+          <div
             className={cn(
-              " px-2 py-4 text-sm overflow-auto whitespace-pre-wrap",
-              "min-[375px]:text-base sm:px-6",
+              'max-w-[90ch] px-2 py-4 text-sm overflow-auto whitespace-pre-wrap',
+              'min-[375px]:text-base sm:px-6'
             )}
           >
-            {children}
-          </p>
+            {formatMessage(children)}
+          </div>
 
           <div
             className={cn(
-              " col-span-2 row-span-1 justify-self-end self-center max-h-[1rem]",
-              "min-[375px]:col-span-1 min-[375px]:self-start",
+              ' col-span-2 row-span-1 justify-self-end self-center max-h-[1rem]',
+              'min-[375px]:col-span-1 min-[375px]:self-start'
             )}
           >
             <Button
-              variant={"custom"}
-              size={"custom"}
+              variant={'custom'}
+              size={'custom'}
               className={cn(
-                "text-muted-foreground opacity-75",
-                "hover:text-destructive active:text-destructive",
+                'text-muted-foreground opacity-75',
+                'hover:text-destructive active:text-destructive'
               )}
             >
               <MinusCircle width="16px" height="16px" />
@@ -58,7 +74,7 @@ const ChatMessage = forwardRef<HTMLElement, ChatMessageProps>(
         <Separator />
       </>
     );
-  },
+  }
 );
 
 export default ChatMessage;
