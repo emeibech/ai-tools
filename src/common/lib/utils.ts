61ebad8994feff699e613b0d51a38e6e222eccc6
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-08 15:40:56 +0800

    Add function for converting entities to characters

--- src/common/lib/utils.ts
+++ src/common/lib/utils.ts
@@ -1,11 +1,17 @@
-import { nanoid } from "@reduxjs/toolkit";
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { nanoid } from '@reduxjs/toolkit';
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
 
-export function generateKeys(list: unknown[]) {
+export function generateKeys(list: unknown[]): string[] {
   return list.map(() => nanoid());
+}
+
+export function convertEntitiesToCharacters(html: string): string | null {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  return doc.documentElement.textContent;
 }
