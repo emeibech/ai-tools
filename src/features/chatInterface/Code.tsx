cd83b5a1e7da93c1f5ef194bc1f06a7acce8f3db
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-11 01:19:46 +0800

    Add logic to extract language and modify style

--- src/features/chatInterface/Code.tsx
+++ src/features/chatInterface/Code.tsx
@@ -5,47 +5,71 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Highlight, themes } from 'prism-react-renderer';
 import { useAppSelector } from '@/app/hooks';
 import { darkModeStatus } from '../darkmode/darkmodeSlice';
+import { cn } from '@/common/lib/utils';
 
 interface CodeProps {
   code: string;
 }
 
+function extractLanguage(code: string): {
+  language: string;
+  codeBlock: string;
+} {
+  const linebreakIndex = code.indexOf('\n');
+  const language = code.slice(0, linebreakIndex);
+  const codeBlock = code.slice(linebreakIndex, -1);
+  return { language, codeBlock };
+}
+
 export default function Code({ code }: CodeProps) {
   const [copied, setCopied] = useState(false);
   const darkmode = useAppSelector(darkModeStatus);
+  const { language, codeBlock } = extractLanguage(code);
 
   return (
     <pre className="whitespace-pre-wrap relative">
-      <span className="flex items-center absolute right-0 top-4">
-        <CopyToClipboard
-          text={code}
-          onCopy={() => {
-            setCopied(true);
-            setTimeout(() => {
-              setCopied(false);
-            }, 2000);
-          }}
-        >
-          <Button
-            size="custom"
-            variant="custom"
-            className="font-sans font-normal px-4"
+      <div
+        className={cn(
+          'flex justify-between bg-secondary text-secondary-foreground',
+          'absolute min-w-full text-sm px-4 py-1 rounded-tr-md rounded-tl-md',
+        )}
+      >
+        <span className="font-sans">{language}</span>
+        <span className="flex items-center">
+          <CopyToClipboard
+            text={code}
+            onCopy={() => {
+              setCopied(true);
+              setTimeout(() => {
+                setCopied(false);
+              }, 2000);
+            }}
           >
-            {copied ? <Check height="16px" /> : <ClipboardCopy height="16px" />}
-            {copied ? 'Copied' : 'Copy code'}
-          </Button>
-        </CopyToClipboard>
-      </span>
+            <Button
+              size="custom"
+              variant="custom"
+              className="font-sans font-normal text-sm"
+            >
+              {copied ? (
+                <Check height="16px" />
+              ) : (
+                <ClipboardCopy height="16px" />
+              )}
+              {copied ? 'Copied' : 'Copy code'}
+            </Button>
+          </CopyToClipboard>
+        </span>
+      </div>
 
       <Highlight
-        language="tsx"
-        theme={darkmode ? themes.vsDark : themes.vsLight}
-        code={code}
+        language={language && 'js'}
+        theme={darkmode ? themes.vsDark : themes.nightOwlLight}
+        code={codeBlock}
       >
         {({ style, tokens, getLineProps, getTokenProps }) => (
           <pre
             style={style}
-            className="p-4 overflow-auto text-xs sm:text-sm rounded"
+            className={cn('p-4 overflow-auto text-xs rounded-md', 'sm:text-sm')}
           >
             {tokens.map((line, i) => (
               <div key={i} {...getLineProps({ line })}>
