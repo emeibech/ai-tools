259d625998d3ff8eda652c56ac2ff4e6d9f46d1b
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-13 04:12:24 +0800

    Create two utility functions
    -scrollToBottom does what its name says: scroll to the bottom of a page
    -mockStreamingApi is used in conjuction with useMockApi hook to stream
      the data in chunks, simulating what chatGPT does

--- src/common/lib/utils.ts
+++ src/common/lib/utils.ts
@@ -15,3 +15,34 @@ export function convertEntitiesToCharacters(html: string): string | null {
   const doc = parser.parseFromString(html, 'text/html');
   return doc.documentElement.textContent;
 }
+
+export function scrollToBottom(): void {
+  window.scrollTo({
+    left: 0,
+    top: document.body.scrollHeight,
+    behavior: 'smooth',
+  });
+}
+
+export const mockStreamingApi = (
+  mockData: unknown[],
+  maxDelay: number = 100,
+) => {
+  return new ReadableStream({
+    start(controller) {
+      let index = 0;
+
+      const pushData = () => {
+        if (index < mockData.length) {
+          controller.enqueue(mockData[index]);
+          index++;
+          setTimeout(pushData, Math.floor(Math.random() * maxDelay));
+        } else {
+          controller.close();
+        }
+      };
+
+      pushData();
+    },
+  });
+};
