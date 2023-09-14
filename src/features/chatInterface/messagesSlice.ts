96bf6f6d070d40a5db8433a0412a1eb1d133d1c5
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-14 23:58:17 +0800

    Create messages slices for each chat-based route

--- src/features/chatInterface/messagesSlice.ts
+++ src/features/chatInterface/messagesSlice.ts
@@ -9,31 +9,71 @@ export interface Messages {
 
 const initialState: Messages[] = [];
 
-export const messagesSlice = createSlice({
-  name: 'messages',
-  initialState,
-  reducers: {
-    messageAdded: (state, action) => {
-      state.push(action.payload);
-    },
-    messageRemoved: (state, action) => {
-      const { id } = action.payload;
-      const index = state.findIndex((message) => message.id === id);
-      if (index !== -1) {
-        state.splice(index, 1);
-      }
-    },
-    messageAppended: (state, action) => {
-      const { id, content } = action.payload;
-      const index = state.findIndex((message) => message.id === id);
-      if (index !== -1) {
-        state[index].content += content;
-      }
-    },
+const reducers = {
+  messageAdded: (state: Messages[], action: { payload: Messages }) => {
+    state.push(action.payload);
   },
+  messageRemoved: (state: Messages[], action: { payload: { id: string } }) => {
+    const { id } = action.payload;
+    const index = state.findIndex((message) => message.id === id);
+    if (index !== -1) {
+      state.splice(index, 1);
+    }
+  },
+  messageAppended: (
+    state: Messages[],
+    action: { payload: { id: string; content: string } },
+  ) => {
+    const { id, content } = action.payload;
+    const index = state.findIndex((message) => message.id === id);
+    if (index !== -1) {
+      state[index].content += content;
+    }
+  },
+};
+
+export const codingMessagesSlice = createSlice({
+  name: 'coding messages',
+  initialState,
+  reducers,
 });
 
-export const { messageAdded, messageRemoved, messageAppended } =
-  messagesSlice.actions;
-export const messages = (state: RootState) => state.messages;
-export default messagesSlice.reducer;
+export const eli5MessagesSlice = createSlice({
+  name: 'eli5 messages',
+  initialState,
+  reducers,
+});
+
+export const generalMessagesSlice = createSlice({
+  name: 'general messages',
+  initialState,
+  reducers,
+});
+
+export const {
+  messageAdded: codingAdded,
+  messageRemoved: codingRemoved,
+  messageAppended: codingAppended,
+} = codingMessagesSlice.actions;
+
+export const {
+  messageAdded: eli5Added,
+  messageRemoved: eli5Removed,
+  messageAppended: eli5Appended,
+} = eli5MessagesSlice.actions;
+
+export const {
+  messageAdded: generalAdded,
+  messageRemoved: generalRemoved,
+  messageAppended: generalAppended,
+} = generalMessagesSlice.actions;
+
+export const codingMessages = (state: RootState) => state.coding;
+export const eli5Messages = (state: RootState) => state.eli5;
+export const generalMessages = (state: RootState) => state.general;
+
+export default {
+  coding: codingMessagesSlice.reducer,
+  eli5: eli5MessagesSlice.reducer,
+  general: generalMessagesSlice.reducer,
+};
