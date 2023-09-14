32b3d02431a62ce131a9cc106e222972cdf2108a
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-15 00:01:20 +0800

    Create functions for getting states and actions
    These functions fetches the correct state or set of actions for the
    corresponding chat interface based on the name parameter passed onto
    ChatInterface component

---
+++ src/features/chatInterface/messagesSliceutils.ts
@@ -0,0 +1,58 @@
+import { Name } from './ChatInterface';
+import {
+  codingMessages,
+  codingAdded,
+  codingAppended,
+  codingRemoved,
+  eli5Messages,
+  eli5Added,
+  eli5Appended,
+  eli5Removed,
+  generalMessages,
+  generalAdded,
+  generalAppended,
+  generalRemoved,
+} from './messagesSlice';
+
+export function getMessagesState(name: Name) {
+  if (name === 'Coding Assistant') return codingMessages;
+  if (name === "Explain Like I'm 5") return eli5Messages;
+  return generalMessages;
+}
+
+export function getMessagesActions(name: Name) {
+  if (name === 'Coding Assistant')
+    return {
+      messageAdded: codingAdded,
+      messageAppended: codingAppended,
+      messageRemoved: codingRemoved,
+    };
+
+  if (name === "Explain Like I'm 5")
+    return {
+      messageAdded: eli5Added,
+      messageAppended: eli5Appended,
+      messageRemoved: eli5Removed,
+    };
+
+  return {
+    messageAdded: generalAdded,
+    messageAppended: generalAppended,
+    messageRemoved: generalRemoved,
+  };
+}
+
+export default {
+  codingMessages,
+  codingAdded,
+  codingAppended,
+  codingRemoved,
+  eli5Messages,
+  eli5Added,
+  eli5Appended,
+  eli5Removed,
+  generalMessages,
+  generalAdded,
+  generalAppended,
+  generalRemoved,
+};
