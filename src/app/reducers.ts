e0b81254591b8ca918539eca98e160e87f760f54
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-16 01:29:06 +0800

    Update rootReducer

--- src/app/reducers.ts
+++ src/app/reducers.ts
@@ -2,9 +2,11 @@ import { combineReducers } from '@reduxjs/toolkit';
 import darkmodeReducer from '../features/darkmode/darkmodeSlice';
 import messagesReducers from '../features/chatInterface/messagesSlice';
 import scrollDirectionReducer from '../features/scrollDirection/scrollDirectionSlice';
+import scrollPositionReducer from '@/features/scrollPosition/scrollPositionSlice';
 
 const rootReducer = combineReducers({
   darkmode: darkmodeReducer,
+  scrollPositions: scrollPositionReducer,
   scrollDirection: scrollDirectionReducer,
   ...messagesReducers,
 });
