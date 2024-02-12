2a4a7f975a663ae1b81406e859bdc4fec90587ef
emeibech <98073805+emeibech@users.noreply.github.com>
2024-02-12 21:52:55 +0800

    Remove PWA
    I tried it because I was curious, but now that I understand it a little
    bit (it's useful for offline usage and getting notification) I realize
    this app doesn't need it. Additionally, It slows down build time by a
    factor of ten, and ios doesn't support it.

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,9 @@
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      manifest: {
-        theme_color: '#08333b',
-        background_color: '#08333b',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-        short_name: 'Emeibech AI',
-        description: 'Emeibech AI',
-        name: 'Emeibech AI',
-        icons: [
-          {
-            src: '/192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: '/512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     reportCompressedSize: false,
   },
