1bc36f0036a8ca45e2a3ec22efae4462f8f3b81a
emeibech <98073805+emeibech@users.noreply.github.com>
2024-02-21 03:22:10 +0800

    Provision free trial access

--- src/common/components/custom/GuestButtons.tsx
+++ src/common/components/custom/GuestButtons.tsx
@@ -1,18 +1,87 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../ui/button';
+import { ReqStatus } from '@/types/routes';
+import { useCallback, useState } from 'react';
+import { useToast } from '../ui/use-toast';
+import { useAppDispatch } from '@/app/hooks';
+import { clientStatusSet } from '@/features/client/clientSlice';
+import { cn, getCatchError } from '@/common/lib/utils';
+
+const baseUrl = import.meta.env.VITE_SERVER_URL;
+const email = import.meta.env.VITE_TRIAL_EMAIL;
+const password = import.meta.env.VITE_TRIAL_PASSWORD;
 
 export default function GuestButtons() {
-  return (
-    <div className="flex gap-6 mt-4 justify-center 2xl:justify-start">
-      <Link to={'/login'}>
-        <Button className="px-8 h-8 xl:h-10 text-base w-[120px]">Login</Button>
-      </Link>
+  const [reqStatus, setReqStatus] = useState<ReqStatus>('idle');
+  const { toast } = useToast();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-      <Link to={'/signup'}>
-        <Button className="px-8 h-8 xl:h-10 text-base w-[120px]">
-          Sign up
-        </Button>
-      </Link>
+  const handleClick = useCallback(async () => {
+    try {
+      const body = { email, password };
+
+      setReqStatus('requesting');
+
+      const response = await fetch(`${baseUrl}/auth/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+        credentials: 'include',
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        setReqStatus('idle');
+        toast({ title: 'Error', description: data.message });
+        return;
+      }
+
+      setReqStatus('success');
+      dispatch(clientStatusSet('user'));
+      navigate('/login');
+    } catch (error) {
+      toast({ title: 'Error', description: getCatchError(error) });
+      setReqStatus('idle');
+    }
+  }, [toast, dispatch, navigate]);
+
+  return (
+    <div
+      className={cn(
+        'grid grid-cols-[repeat(auto-fill,_minmax(120px,_1fr))] gap-8',
+        '',
+      )}
+    >
+      <Button className="h-8 p-0 xl:h-10 text-base">
+        <Link
+          to={'/login'}
+          className="min-w-full min-h-full grid place-content-center"
+        >
+          <p>Login</p>
+        </Link>
+      </Button>
+
+      <Button className="h-8 p-0 xl:h-10 text-base">
+        <Link
+          to={'/signup'}
+          className="min-w-full min-h-full grid place-content-center"
+        >
+          <p>Sign up</p>
+        </Link>
+      </Button>
+
+      <Button
+        onClick={handleClick}
+        disabled={reqStatus === 'requesting'}
+        className={cn(
+          'px-8 h-8 mx-20 xl:h-10 text-base col-span-2',
+          'min-[456px]:col-span-1 min-[456px]:mx-0',
+        )}
+      >
+        Try it!
+      </Button>
     </div>
   );
 }
